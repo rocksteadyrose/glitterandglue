f30fbc604770741ebbf2159e844e25389e95f6a0
Leslie Rose <leslierose@Leslies-MBP-2.attlocal.net>
2019-06-18 16:50:56 -0500

    cors

--- js/contact_me.js
+++ js/contact_me.js
@@ -1,10 +1,10 @@
-jQuery.ajaxPrefilter(function(options) {
-  if (options.crossDomain && jQuery.support.cors) {
-      options.url = 'https://cors-anywhere.herokuapp.com/' + options.url;
-  }
-});
-
 $(function() {
+
+  jQuery.ajaxPrefilter(function(options) {
+    if (options.crossDomain && jQuery.support.cors) {
+        options.url = 'https://cors-anywhere.herokuapp.com/' + options.url;
+    }
+  });
 
   $("#contactForm input,#contactForm textarea").jqBootstrapValidation({
     preventSubmit: true,
